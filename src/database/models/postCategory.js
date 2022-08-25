bbc031ee9970d7838e9291235b4f732a70654dda
ElaineCosta <elainerscosta@hotmail.com>
2022-08-25 18:40:04 -0300

    :feat: criando o model de PostCategory com foreingKey e fazendo as associate das table Categories e BlogPosts

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,47 @@
+/**
+* @param {import('sequelize').Sequelize } Sequelize
+* @param {import('sequelize').DataTypes} DataTypes
+* @returns
+*/
+
+const PostCategory = (sequelize, DataTypes) => {
+
+  const postCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      foreignKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      foreignKey: true,
+    },
+  },
+  { timestamps: false,
+    underscored: true,
+    tableName: 'PostCategories',
+  },
+  );
+
+  postCategory.associate = (models) => {
+
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'Categories',
+      through: 'PostCategory',
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    })
+  
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'BlogPosts', 
+      through: 'PostCategory',
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    })
+
+  }
+  return postCategory;
+};
+
+module.exports = PostCategory;
