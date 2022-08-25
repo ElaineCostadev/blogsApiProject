91ec24a46b183cc15dcffea5b3935792adeb98a8
ElaineCosta <elainerscosta@hotmail.com>
2022-08-25 18:35:40 -0300

    :feat: criando a migration do sequelize para o user e ajustando as informações que serão obrigatorias na table

---
+++ src/database/migrations/20220825182306-create-user.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+/**
+* @param {import('sequelize').QueryInterface} queryInterface
+* @param {import('sequelize').Sequelize } Sequelize
+*/
+
+// validations Sequelize = https://sequelize.org/docs/v6/core-concepts/validations-and-constraints/
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      displayName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true,
+        validate: {
+          // isEmail: true,
+          isEmail: {
+          msg: 'Please enter your email'
+          }
+        }
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
