e35465350658c0f36078292b993c45715e9fecd9
ElaineCosta <elainerscosta@hotmail.com>
2022-08-25 18:34:50 -0300

    :feat: criando a camada de model com sequelize, inserindo as informações obrigatorias e as associate necessarias

---
+++ src/database/models/user.js
@@ -0,0 +1,31 @@
+/**
+* @param {import('sequelize').Sequelize } Sequelize
+* @param {import('sequelize').DataTypes} DataTypes
+* @returns
+*/
+
+const User = (sequelize, DataTypes) => {
+  const user = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  },
+  {
+    timestamps: false,
+    tableName: 'Users',
+    underscored: true,
+});
+
+user.associate = (models) => {
+  user.hasMany(models.BlogPost, { foreignKey: 'userId', as: 'BlogPosts' })
+  // essa FK deve ser o nome que está no outro model ou o que está nesse? seria id?
+  // passou no teste com as duas opções userId e id
+  // esse as, deve ser minusculo?
+}
+
+  return user;
+};
+
+module.exports = User;
