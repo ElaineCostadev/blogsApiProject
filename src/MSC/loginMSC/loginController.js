b28e4b0411611b601e283bc652384996d0830daa
ElaineCosta <elainerscosta@hotmail.com>
2022-08-26 20:11:26 -0300

    :feat: criando funcao de loginController

---
+++ src/MSC/loginMSC/loginController.js
@@ -0,0 +1,17 @@
+const loginService = require('./loginService');
+
+const loginController = {
+  login: async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const result = await loginService.login(email, password);
+      res.status(200).json({ token: result });
+    } catch (error) {
+      return res
+      .status(400)
+      .json({ message: error.message });
+    }
+  },
+};
+
+module.exports = loginController;
