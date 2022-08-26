c092b9a45191aed3c1f994af322d2b5c26747e99
ElaineCosta <elainerscosta@hotmail.com>
2022-08-26 20:05:40 -0300

    :fix: incluindo autoIncremet no id

--- src/database/models/category.js
+++ src/database/models/category.js
@@ -6,7 +6,7 @@
 
 const Category = (sequelize, DataTypes) => {
   const category = sequelize.define('Category', {
-    id: { type: DataTypes.INTEGER, primaryKey: true },
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: DataTypes.STRING,
   }, {
     timestamps: false,
