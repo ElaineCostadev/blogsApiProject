df297379632c0548b9709d46949f1b60ac99289a
ElaineCosta <elainerscosta@hotmail.com>
2022-08-26 20:13:25 -0300

    inclundo alteracoes launch e json

--- src/api.js
+++ src/api.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const routes = require('./routes');
+require('express-async-errors');
 
+const errorMiddleware = require('./errors/errorMiddleware');
 // ...
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/login', routes.loginRoute);
+
+app.use(errorMiddleware);
 // ...
 
 // É importante exportar a constante `app`,
