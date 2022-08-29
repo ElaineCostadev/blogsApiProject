244612cd1ceeb99e99dac628857994bfdab52135
ElaineCosta <elainerscosta@hotmail.com>
2022-08-29 16:41:45 -0300

    :feat: incluindo a funcao getByPK no userController para responder ao cliente com try e catch

--- src/MSC/userMSC/userController.js
+++ src/MSC/userMSC/userController.js
@@ -2,13 +2,23 @@ const userService = require('./userService');
 
 const userController = {
   getAll: async (_req, res) => {
-      const result = await userService.getAll();
+    const result = await userService.getAll();
+    res.status(200).json(result);
+  },
+
+  getByPk: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const result = await userService.getByPk(id);
       res.status(200).json(result);
+    } catch (error) {
+      return res.status(error.status)
+      .json({ message: error.message });
+    }
   },
 
   create: async (req, res) => {
     const { displayName, email, password, image } = req.body;
-    console.log(req.body, 'Controller');
     try {
       const result = await userService.create({ displayName, email, password, image });
       res.status(201).json({ token: result });
