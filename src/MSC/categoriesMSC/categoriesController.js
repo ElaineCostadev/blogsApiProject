00403e4908e43ba000462dddc157e9d69ba8c369
ElaineCosta <elainerscosta@hotmail.com>
2022-08-29 17:10:22 -0300

    :feat: criando funcao getAll no Controller para retornar o status 200 ao cliente

--- src/MSC/categoriesMSC/categoriesController.js
+++ src/MSC/categoriesMSC/categoriesController.js
@@ -1,10 +1,15 @@
 const categoriesService = require('./categoriesService');
 
 const categoriesController = {
+  getAll: async (req, res) => {
+    const allCategories = await categoriesService.getAll();
+    return res.status(200).json(allCategories);
+  },
+
   create: async (req, res) => {
     const { name } = req.body;
     const category = await categoriesService.create(name);
-    res.status(201).json(category);
+    return res.status(201).json(category);
   },
 };
 
