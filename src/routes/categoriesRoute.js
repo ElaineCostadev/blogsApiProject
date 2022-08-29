d3203181710f896408bf5f76e591dcb995f71083
ElaineCosta <elainerscosta@hotmail.com>
2022-08-29 17:09:56 -0300

    :feat: incluindo rota get para pegar todas as categorias no meu BD

--- src/routes/categoriesRoute.js
+++ src/routes/categoriesRoute.js
@@ -6,11 +6,8 @@ const categoriesController = require('../MSC/categoriesMSC/categoriesController'
 const validateCategory = require('../middlewares/validateCategory');
 const tokenValidation = require('../middlewares/tokenValidation');
 
-// ao criar um novo usuario eu não tenho ainda o token - 
 categoriesRoute.post('/', tokenValidation, validateCategory, categoriesController.create);
 
-// depois que ja criei sou obrigada a ter o token para ter acesso as informações
-// categoriesRoute.get('/', tokenValidation, categoriesController.getAll);
-// categoriesRoute.get('/:id', tokenValidation, categoriesController.getByPk);
+categoriesRoute.get('/', tokenValidation, categoriesController.getAll);
 
 module.exports = categoriesRoute;
