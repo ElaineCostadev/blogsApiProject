148d260d76d70c5a69ef20461891478fd2391ad9
ElaineCosta <elainerscosta@hotmail.com>
2022-08-29 15:39:25 -0300

    :fix: ajustando uncao de middleware em tokenvalidation - com a expiracao do token

--- src/middlewares/tokenValidation.js
+++ src/middlewares/tokenValidation.js
@@ -1,11 +1,13 @@
 const jwtSecret = process.env.JWT_SECRET;
+const CustomError = require('../errors/CustomError');
 const jwtHelpers = require('../helpers/jwtHelpers');
 require('dotenv/config');
 
 const tokenValidation = (req, _res, next) => {
   const { authorization } = req.headers;
   const data = jwtHelpers.validateToken(authorization, jwtSecret);
-  req.email = data;
+  if (!data) throw new CustomError('401', 'Expired or invalid token');
+  // req.email = data;
   next();
 };
 
