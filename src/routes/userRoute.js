af66132c6e04935e374d25f660c53577d125e3bd
ElaineCosta <elainerscosta@hotmail.com>
2022-08-29 15:41:25 -0300

    :feat: incluindo rota de get no user

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -4,7 +4,9 @@ const userRoute = express.Router();
 
 const userController = require('../MSC/userMSC/userController');
 const validateUser = require('../middlewares/validateUser');
+const tokenValidation = require('../middlewares/tokenValidation');
 
+userRoute.get('/', tokenValidation, userController.getAll);
 userRoute.post('/', validateUser, userController.create);
 
 module.exports = userRoute;
