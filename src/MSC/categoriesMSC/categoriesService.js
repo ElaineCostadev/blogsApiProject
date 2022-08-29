120143a91852a06ff076f2c6ff3b8f36ea311077
ElaineCosta <elainerscosta@hotmail.com>
2022-08-29 17:10:55 -0300

    :feat: criando funcao getAll no service para pegar todas as informações no BD com a funcao findAll do Sequelize

--- src/MSC/categoriesMSC/categoriesService.js
+++ src/MSC/categoriesMSC/categoriesService.js
@@ -1,6 +1,8 @@
 const { Category } = require('../../database/models');
 
 const categoriesService = {
+  getAll: async () => Category.findAll(),
+
   create: async (name) => {
     const result = await Category.create({ name });
     return result;
