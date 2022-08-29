63c049f963d0d68a141ea622df71a924ae835418
ElaineCosta <elainerscosta@hotmail.com>
2022-08-29 17:03:16 -0300

    :feat: incluindo funcao create na camada Controller para cadatrar uma nova categoria na table Category no BD

---
+++ src/MSC/categoriesMSC/categoriesController.js
@@ -0,0 +1,11 @@
+const categoriesService = require('./categoriesService');
+
+const categoriesController = {
+  create: async (req, res) => {
+    const { name } = req.body;
+    const category = await categoriesService.create(name);
+    res.status(201).json(category);
+  },
+};
+
+module.exports = categoriesController;
