a3eaada524c37c794462257203501ce710024efb
ElaineCosta <elainerscosta@hotmail.com>
2022-08-30 20:03:49 -0300

    :fix: retirar os logs e ajustando o retorno na funcao generateToken

--- src/MSC/loginMSC/loginService.js
+++ src/MSC/loginMSC/loginService.js
@@ -6,13 +6,13 @@ const loginService = {
   login: async (email, password) => {
     const user = await User.findOne({
       where: { email },
-      logging: console.log,
+      // logging: console.log,
       attributes: ['email', 'password'],
       raw: true,
     });
-      console.log(user);
+      // console.log(user);
       if (!user || user.password !== password) throw new CustomError('400', 'Invalid fields');
-    const token = generateToken(user.email);
+    const token = generateToken({ email });
 
     return token;
   },
