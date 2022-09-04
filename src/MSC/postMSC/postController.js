69af5f81a5cad86a3fb9d917c41b050112b9e2c3
ElaineCosta <elainerscosta@hotmail.com>
2022-09-04 19:03:34 -0300

    :feat: criando funcao search no controller

--- src/MSC/postMSC/postController.js
+++ src/MSC/postMSC/postController.js
@@ -20,10 +20,10 @@ const postController = {
     try {
       // peguei o email que vem do token na rota do login - tokenValidation
       const { email } = req.email;
-      // console.log(email, 'email Controller');
+
       const { title, content, categoryIds, userId } = req.body;
       const result = await postService.create({ title, content, categoryIds, userId, email });
-      // console.log(result, 'result do Controller');
+
       res.status(201).json(result);
     } catch (error) {
       return res.status(400).json({ message: '"categoryIds" not found' });
@@ -52,6 +52,12 @@ const postController = {
     } catch (error) {
       return res.status(error.status).json({ message: error.message });
     }
+  },
+
+  search: async (req, res) => {
+    const title = req.query.q;
+    const result = await postService.search(title);
+    return res.status(200).json(result);
   },
 };
 
