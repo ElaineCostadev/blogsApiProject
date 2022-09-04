8073e8756a2035bde475116ff60c6bef1b967eca
ElaineCosta <elainerscosta@hotmail.com>
2022-09-04 16:13:39 -0300

    :feat: incluindo nova rota para deletar o usuario

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -12,5 +12,6 @@ userRoute.post('/', validateUser, userController.create);
 // depois que ja criei sou obrigada a ter o token para ter acesso as informações
 userRoute.get('/', tokenValidation, userController.getAll);
 userRoute.get('/:id', tokenValidation, userController.getByPk);
+userRoute.delete('/me', tokenValidation, userController.destroy);
 
 module.exports = userRoute;
