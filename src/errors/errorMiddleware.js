7bb9754a949037339ccbcc5eccc04f1de31993f1
ElaineCosta <elainerscosta@hotmail.com>
2022-09-04 14:20:56 -0300

    :fix: ajustando retorno do middleware de erro

--- src/errors/errorMiddleware.js
+++ src/errors/errorMiddleware.js
@@ -1,6 +1,6 @@
-const errorMiddleware = (err, _req, res, _next) =>
-    res.status(err.status || 500).json({
-    message: err.message,
+const errorMiddleware = (error, _req, res, _next) =>
+    res.status(error.status || 500).json({
+    message: error.message,
   });
   
 module.exports = errorMiddleware;
