9bdf0a498a8432a357b6fa2d714822afe52f5aa6
ElaineCosta <elainerscosta@hotmail.com>
2022-09-04 18:53:38 -0300

    :fix: retirando o arquivo searchRoute

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,5 @@ const loginRoute = require('./loginRoute');
 const userRoute = require('./userRoute');
 const categoriesRoute = require('./categoriesRoute');
 const postRoute = require('./postRoute');
-const searchRoute = require('./searchRoute');
 
-module.exports = { loginRoute, userRoute, categoriesRoute, postRoute, searchRoute };
+module.exports = { loginRoute, userRoute, categoriesRoute, postRoute };
